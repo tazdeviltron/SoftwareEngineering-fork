51be8640bcfc149b526ecb282dbee4f3df891f92
tazdeviltron <149820174+tazdeviltron@users.noreply.github.com>
2023-12-01 19:47:04 +0000

    finished

--- Tasks/01 - Variables and Arithmetic/121-scanf/121-scanf.cpp
+++ Tasks/01 - Variables and Arithmetic/121-scanf/121-scanf.cpp
@@ -4,18 +4,26 @@ int main()
 {
     //Create a variable x
     int x = 0;
+    int y = 0;
+    int z = 0;
 
     //Prompt user
     printf("Enter your age\n");
+    printf("Enter another number\n");
 
     //Read keyboard response - look for an integer
     int found = scanf_s("%d", &x);
+    int number = scanf_s("%d", &y);
+    int average = (x+y)/2;
 
     //Display how many items were matched
     printf("Matched %d items\n", found);
+    printf("Matched %d items\n", number);
 
     //Display what was typed in
     printf("x = %d\n", x);
+    printf("y = %d\n", y);
+    printf("average= %d\n", average);
 }
 
 
