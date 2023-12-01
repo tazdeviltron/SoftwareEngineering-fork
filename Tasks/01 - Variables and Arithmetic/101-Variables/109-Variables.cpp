446ec232ac38a844fa4f9745916cf5855f146f50
tazdeviltron <149820174+tazdeviltron@users.noreply.github.com>
2023-12-01 18:16:43 +0000

    Update 109-Variables.cpp

--- Tasks/01 - Variables and Arithmetic/101-Variables/109-Variables.cpp
+++ Tasks/01 - Variables and Arithmetic/101-Variables/109-Variables.cpp
@@ -4,7 +4,7 @@ int main()
 { // Block opening brace
 
 	char c = 88;				// Character (1 byte)
-	short year = 2023;			// Short integer
+	unsigned short year = 2023;			// Short integer
 	int age = 23;				// Integer
 	long bigNumber = 123456789;	// Long integer
 	long long veryBigNumber = 1LL << 30;	//2^30 - challenge (advanced), why have I put 1LL?
@@ -27,7 +27,7 @@ int main()
 	//Overflow
 	year = 32767;
 	year = year + 1;
-	printf("year = %d\n", year);
+	printf("year = %u\n", year);
 
 	//Divide
 	age = 50;
