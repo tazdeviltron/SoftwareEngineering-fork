608c803a051c71ddfeeb9a61b2d93f0df851022e
tazdeviltron <149820174+tazdeviltron@users.noreply.github.com>
2023-12-01 18:23:09 +0000

    finished

--- Tasks/01 - Variables and Arithmetic/111-shiftingbits/111-shiftingbits.cpp
+++ Tasks/01 - Variables and Arithmetic/111-shiftingbits/111-shiftingbits.cpp
@@ -29,8 +29,10 @@ int main()
 	//TASKS:
 
 	//Shift right by 1 and display
-
+	number >>= 1;
+	displayAndCompare(number);
 
 	//Shift left by 1 and display
-
+	number <<= 1;
+	displayAndCompare(number);
 }
